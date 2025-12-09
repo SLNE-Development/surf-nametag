bfad36c45d1bc3aa9956c7810a36f5062850ab9b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-12-09 17:57:08 +0100

    feat: add GitHub Actions workflow for Maven publishing and release creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import dev.slne.surf.surfapi.gradle.util.slneReleases
+
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -11,4 +13,16 @@ buildscript {
 allprojects {
     version = findProperty("version") as String
     group = "dev.slne.surf.nametag"
+}
+
+subprojects {
+    afterEvaluate {
+        plugins.withType<PublishingPlugin> {
+            configure<PublishingExtension> {
+                repositories {
+                    slneReleases()
+                }
+            }
+        }
+    }
 }
