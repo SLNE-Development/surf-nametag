dd4c90568c06a1fa046146ed40a4e2267dbb6914
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-20 14:47:17 +0200

    refactor: update version to 1.0.7-SNAPSHOT and add nametag visibility handling

--- surf-nametag-paper/src/main/kotlin/dev/slne/surf/nametag/paper/listener/NametagListener.kt
+++ surf-nametag-paper/src/main/kotlin/dev/slne/surf/nametag/paper/listener/NametagListener.kt
@@ -3,8 +3,11 @@ package dev.slne.surf.nametag.paper.listener
 import dev.slne.surf.nametag.paper.service.nametagService
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerHideEntityEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.event.player.PlayerShowEntityEvent
+import org.bukkit.entity.Player
 
 object NametagListener : Listener {
     @EventHandler
@@ -15,5 +18,17 @@ object NametagListener : Listener {
     @EventHandler
     fun onQuit(event: PlayerQuitEvent) {
         nametagService.handleQuit(event.player)
+    }
+
+    @EventHandler
+    fun onHide(event: PlayerHideEntityEvent) {
+        val hiddenPlayer = event.entity as? Player ?: return
+        nametagService.hideNametag(hiddenPlayer.uniqueId, event.player.uniqueId)
+    }
+
+    @EventHandler
+    fun onShow(event: PlayerShowEntityEvent) {
+        val shownPlayer = event.entity as? Player ?: return
+        nametagService.showNametag(shownPlayer.uniqueId, event.player.uniqueId)
     }
 }
