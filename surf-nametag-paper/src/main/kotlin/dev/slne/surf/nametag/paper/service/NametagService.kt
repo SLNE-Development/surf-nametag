b64154de78b0a572b0675b47eabe9d8cfb4fd25f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-06-18 15:06:18 +0200

    refactor: update version to 1.0.10 and add nametag visibility handling

--- surf-nametag-paper/src/main/kotlin/dev/slne/surf/nametag/paper/service/NametagService.kt
+++ surf-nametag-paper/src/main/kotlin/dev/slne/surf/nametag/paper/service/NametagService.kt
@@ -90,10 +90,15 @@ class NametagService {
 
     fun handlePlayerUntracked(playerEntityId: Int, viewer: Player) {
         val player = Bukkit.getOnlinePlayers().find { it.entityId == playerEntityId } ?: return
+        println("Found ${player.name}")
         val key = player.uniqueId to viewer.uniqueId
-        if (key in spawnedDisplays) {
+        println("Spawned displays ${spawnedDisplays.size}")
+
+        spawnedDisplays.forEach { _ ->
+            println("Removing nametag for ${player.name} and viewer ${viewer.name}")
             spawnedDisplays.remove(key)
             val textDisplayEntityId = entityIds[player.uniqueId] ?: return
+            println("Destroying entity with ID $textDisplayEntityId for viewer ${viewer.name}")
             viewer.sendPacket(WrapperPlayServerDestroyEntities(textDisplayEntityId))
         }
     }
