1cb1d44bbf86f44e05b9274a3cd9653677e209c3
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-07-20 12:23:50 +0200

    refactor: implement preventNametagUpdate using NametagService

--- surf-nametag-paper/src/main/kotlin/dev/slne/surf/nametag/paper/api/SurfNametagApiImpl.kt
+++ surf-nametag-paper/src/main/kotlin/dev/slne/surf/nametag/paper/api/SurfNametagApiImpl.kt
@@ -2,12 +2,14 @@ package dev.slne.surf.nametag.paper.api
 
 import com.google.auto.service.AutoService
 import dev.slne.surf.nametag.api.SurfNametagApi
+import dev.slne.surf.nametag.paper.service.NametagService
 import net.kyori.adventure.util.Services
 import java.util.*
 
 @AutoService(SurfNametagApi::class)
 class SurfNametagApiImpl : SurfNametagApi, Services.Fallback {
     override fun preventNametagUpdate(playerUuid: UUID): Boolean {
-        TODO("Not yet implemented")
+        NametagService.preventUpdates(playerUuid)
+        return true
     }
 }
